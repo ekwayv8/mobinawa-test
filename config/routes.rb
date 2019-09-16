bd6c221b54ccc30c1ed45640dff2bc477ce3227b
ekwayv8 <yvon.ekwalla@gmail.com>
2019-09-16 11:28:57 +0200

    Modification of routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :admins do
     resources :companies
     resources :products
-    get '/dashboard', to: 'admins#dashboard', as: 'dashboard'
+    #get '/dashboard', to: 'admins#dashboard', as: 'dashboard'
   end
 
   resources :countries 
