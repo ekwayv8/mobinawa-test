c025a3532da218f214b28d6f4532dc237dbb2c54
ekwayv8 <yvon.ekwalla@gmail.com>
2019-09-16 13:05:29 +0200

    Adding option in the production environment in order to enable devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,11 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+    # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.default_url_options = { host: 'https://mobinawa.herokuapp.com/', port: 3000 }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
